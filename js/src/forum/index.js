baa9f7e6534606082ac12c7437672ea222f94d94
songran shi <kiddo>
2023-04-18 23:57:57 +0800

    fix bug

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -5,8 +5,8 @@ import ItemList from 'flarum/common/utils/ItemList';
 import RankBadge from './components/RankBadge';
 import Badge from 'flarum/components/Badge';
 
-app.initializers.add('datlechin/flarum-traditional-rank-icons', () => {
-  extend(User.prototype, 'badges', function () {
+app.initializers.add('kiddo/flarum-traditional-rank-icons', () => {
+  extend(User.prototype, 'badges', function (badges) {
     const items = new ItemList();
     const groups = this.groups();
 
